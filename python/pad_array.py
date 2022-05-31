10bf3e7fbf3d174324486cecb3261abdc6d90009
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-31 01:11:00 -0500

    Completed algo

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,10 @@
-#REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    
+    if(len(array) >= min_size):
+        return array
+    else:
+        for i in range(len(array), min_size):
+            if(len(array) < min_size):
+                array.append(value)
+
+    return array
