4e8b1a5f62ebeb692246b513029078c0b6d856a9
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-31 01:22:56 -0500

    Completed algo

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,14 @@
-// REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
+exports.pad = (array, minSize, value=null) => {
 
+    if(array.length >= minSize) {
+        return array;
+    } else {
+        for(let i=array.length; i < minSize; i++) {
+            if(array.length < minSize) {
+                array.push(value);
+            }
+        }
+    }
+
+    return array;
 }
--- js/padArraySpec.js
+++ js/padArraySpec.js
@@ -1,1 +1,6 @@
-// Write unit tests!
+var pad = require("./padArray");
+
+console.log(JSON.stringify(pad.pad([1,2,3], 5)) === JSON.stringify([1,2,3,null,null]));
+console.log(JSON.stringify(pad.pad([1,2,3], 5, 'apple')) === JSON.stringify([1,2,3,'apple','apple']));
+console.log(JSON.stringify(pad.pad([1,2,3], 3)) === JSON.stringify([1,2,3]));
+console.log(JSON.stringify(pad.pad([1,2,3], 0)) === JSON.stringify([1,2,3]));
